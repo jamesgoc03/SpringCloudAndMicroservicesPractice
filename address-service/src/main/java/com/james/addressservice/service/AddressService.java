cc973cd39ed0fc911b8028e5e141ba69f6b5e6ae
James Alejandro Gómez Cuervo <jamesgoc03gmail.com>
2024-10-28 15:21:25 -0500

    06 The Client side load balancing was implemented with Spring cloud load balancer

--- address-service/src/main/java/com/james/addressservice/service/AddressService.java
+++ address-service/src/main/java/com/james/addressservice/service/AddressService.java
@@ -6,6 +6,8 @@ import com.james.addressservice.Presentation.dto.CreateAddressRequest;
 import com.james.addressservice.helper.mapper.AddressMapper;
 import com.james.addressservice.persistence.model.Address;
 import com.james.addressservice.persistence.repository.AddressRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +17,9 @@ import java.util.Optional;
 @Service
 public class AddressService {
 
-
 	@Autowired
 	private AddressRepository addressRepository;
+	private static final Logger logger = LoggerFactory.getLogger(AddressService.class);
 
 	public AddressResponse createAddress(CreateAddressRequest createAddressRequest) {
 		Address address = Address.builder()
@@ -32,6 +34,7 @@ public class AddressService {
 		Optional<Address> address = addressRepository.findById(id);
 		if(address.isEmpty())
 			throw new NoSuchElementException("The address with id " + id + " does not exist.");
+		logger.warn("Hello from this instance");
 		return AddressMapper.mapModelToResponse(address.get());
 	}
 
