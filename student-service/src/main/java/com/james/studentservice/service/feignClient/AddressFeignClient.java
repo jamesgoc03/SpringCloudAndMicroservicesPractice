e0e269494c4d658128f9cea5c5f312a41d29487a
James Alejandro Gómez Cuervo <jamesgoc03@gmail.com>
2024-10-28 20:19:27 -0500

    10 The fault tolerance and circuit breaker was implemented with resilience4j

--- student-service/src/main/java/com/james/studentservice/service/feignClient/AddressFeignClient.java
+++ student-service/src/main/java/com/james/studentservice/service/feignClient/AddressFeignClient.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(url = "${address.service.url}", value = "addressFeignClient", path = "/api/address")
-@FeignClient(value = "address-service", path = "/api/address")
+//@FeignClient(value = "address-service", path = "/api/address")
+@FeignClient(value = "api-gateway")
 public interface AddressFeignClient {
 
-    @GetMapping("/getById/{id}")
+    @GetMapping("/address-service/api/address/getById/{id}")
     public AddressResponse getById (@PathVariable long id);
 
 }
