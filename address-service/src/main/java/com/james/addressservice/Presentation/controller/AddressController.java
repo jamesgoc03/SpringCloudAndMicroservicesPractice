8c8c501da171ad2c6e6fdf99721d7b47047cadda
James Alejandro Gómez Cuervo <jamesgoc03gmail.com>
2024-10-29 22:14:49 -0500

    11 The config server was implemented and configured only in address service to test it

--- address-service/src/main/java/com/james/addressservice/Presentation/controller/AddressController.java
+++ address-service/src/main/java/com/james/addressservice/Presentation/controller/AddressController.java
@@ -4,14 +4,20 @@ import com.james.addressservice.Presentation.dto.AddressResponse;
 import com.james.addressservice.Presentation.dto.CreateAddressRequest;
 import com.james.addressservice.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/address")
+@RefreshScope
 public class AddressController {
 
     @Autowired
     private AddressService addressService;
+
+    @Value("${address.test}")
+    private String test;
 
     @PostMapping("/create")
     public AddressResponse createAddress (@RequestBody CreateAddressRequest createAddressRequest) {
@@ -21,6 +27,11 @@ public class AddressController {
     @GetMapping("getById/{id}")
     public AddressResponse getById (@PathVariable long id) {
         return addressService.getById(id);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return test;
     }
 
 }
